eb25dcadb5a34410e8965bfbe6d66bd5ba6ff23c
aidataml <52681210+aidataml@users.noreply.github.com>
2023-09-09 09:24:19 -0400

    Initial upload of Memory Game

--- script.js
+++ script.js
@@ -1,36 +1,136 @@
-/* Write a function called countdown that accepts a number as a parameter and every 1000 milliseconds 
-decrements the value and console.logs it. Once the value is 0 it should log DONE! and stop.*/
+const gameContainer = document.getElementById("game");
+let card1 = null;
+let card2 = null;
+let cardsFlipped = 0;
+let noClicking = false;
 
-function countdown(number){
-  let countdownTimer = setInterval(function() {
-    number = number - 1;
+const COLORS = [
+  "red",
+  "blue",
+  "green",
+  "orange",
+  "purple",
+  "yellow",
+  "white",
+  "deeppink",
+  "aqua",
+  "red",
+  "blue",
+  "green",
+  "orange",
+  "purple",
+  "yellow",
+  "white",
+  "deeppink",
+  "aqua"
+];
 
-  if (number <= 0) {
-    clearInterval(countdownTimer);
-    console.log("DONE!");
-} else {
-    console.log(number);
-    }
-},1000)
+// here is a helper function to shuffle an array
+// it returns the same array with values shuffled
+// it is based on an algorithm called Fisher Yates if you want ot research more
+function shuffle(array) {
+  let counter = array.length;
+
+  // While there are elements in the array
+  while (counter > 0) {
+    // Pick a random index
+    let index = Math.floor(Math.random() * counter);
+
+    // Decrease counter by 1
+    counter--;
+
+    // And swap the last element with it
+    let temp = array[counter];
+    array[counter] = array[index];
+    array[index] = temp;
+  }
+
+  return array;
 }
 
-/* Write a function called randomGame that selects a random number between 0 and 1 every 1000 milliseconds 
-and each time that a random number is picked, add 1 to a counter. If the number is greater than .75, stop 
-the timer and console.log the number of tries it took before we found a number greater than .75.*/
+let shuffledColors = shuffle(COLORS);
 
-function randomGame(){
-    let counter = 0;
+// this function loops over the array of colors
+// it creates a new div and gives it a class with the value of the color
+// it also adds an event listener for a click for each card
+function createDivsForColors(colorArray) {
+  for (let color of colorArray) {
+    // create a new div
+    const newDiv = document.createElement("div");
 
-  let randomTimer = setInterval(function() {
-    number = Math.random();
-    counter = counter + 1;
+    // give it a class attribute for the value we are looping over
+    newDiv.classList.add(color);
 
-  if (number > 0.75) {
-    console.log(number);
-    clearInterval(randomTimer);
-    console.log("It took " + counter + " times to find a number greater than 0.75.");
-} else {
-    console.log(number);
-    }
-},1000)
+    // call a function handleCardClick when a div is clicked on
+    newDiv.addEventListener("click", handleCardClick);
+
+    // append the div to the element with an id of game
+    gameContainer.append(newDiv);
+  }
+}
+
+// TODO: Implement this function!
+function handleCardClick(e) {
+
+  // If there is no click, end function. 
+  if (noClicking) return;	
+  if (e.target.classList.contains("flipped")) return;
+  
+  // Use event.target to see which element was clicked.
+  let currentCard = e.target;
+
+  // Set background color of the clicked card.
+  currentCard.style.backgroundColor = currentCard.classList[0];
+
+  // Set clicked cards to "flipped".
+  if (!card1 || !card2) {
+    currentCard.classList.add("flippedCard");
+    card1 = card1 || currentCard;
+    card2 = currentCard === card1 ? null : currentCard;
+  }
+
+  // If both cards match.
+  if (card1 && card2) {
+    noClicking = true;
+
+    let match1 = card1.className;
+    let match2 = card2.className;
+
+  if (match1 === match2) {
+    cardsFlipped += 2;
+    card1.removeEventListener("click", handleCardClick);
+    card2.removeEventListener("click", handleCardClick);
+    card1 = null;
+    card2 = null;
+    noClicking = false;
+  } else {
+    setTimeout(function() {
+      card1.style.backgroundColor = "";
+      card2.style.backgroundColor = "";
+      card1.classList.remove("flippedCard");
+      card2.classList.remove("flippedCard");
+      card1 = null;
+      card2 = null;
+      noClicking = false;
+    }, 1000);
+  }
+}
+  // If all cards are matched appropriately, alert "well done".
+  if (cardsFlipped === COLORS.length) alert("Well done! You found all the matches!");
+}
+
+// when the DOM loads
+createDivsForColors(shuffledColors);
+
+function restartGame() {
+  // Reset the game.
+  gameContainer.innerHTML = '';
+  card1 = null;
+  card2 = null;
+  cardsFlipped = 0;
+  noClick = false;
+  shuffledColors = shuffle(COLORS);
+
+  // Reload the game.
+  createDivsForColors(shuffledColors);
 }
