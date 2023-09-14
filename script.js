5f01e7815ba242b0e6a7894a25523272f98311f1
aidataml <52681210+aidataml@users.noreply.github.com>
2023-09-14 16:44:20 -0400

    Initial upload of rest-spread exercise.

--- script.js
+++ script.js
@@ -1,36 +1,138 @@
-/* Write a function called countdown that accepts a number as a parameter and every 1000 milliseconds 
-decrements the value and console.logs it. Once the value is 0 it should log DONE! and stop.*/
+/* Write an ES2015 Version 
 
-function countdown(number){
-  let countdownTimer = setInterval(function() {
-    number = number - 1;
+function filterOutOdds() {
+    var nums = Array.prototype.slice.call(arguments);
+    return nums.filter(function(num) {
+      return num % 2 === 0
+    });
+  }
+*/
 
-  if (number <= 0) {
-    clearInterval(countdownTimer);
-    console.log("DONE!");
-} else {
-    console.log(number);
-    }
-},1000)
+const filterOutOdds = (...args) => args.filter((num) => num % 2 === 0);
+// console.log(filterOutOdds(1,2,3,4,5,6,7)) // returns [2,4,6]
+
+
+/* ## **findMin**
+
+Write a function called findMin that accepts a variable number of arguments and returns the 
+smallest argument.
+
+Make sure to do this using the rest and spread operator.*/
+
+function findMin(...args) {
+    return Math.min(...args);
 }
+// console.log(findMin(1,2,0,-1,2,5,6)) // returns -1
 
-/* Write a function called randomGame that selects a random number between 0 and 1 every 1000 milliseconds 
-and each time that a random number is picked, add 1 to a counter. If the number is greater than .75, stop 
-the timer and console.log the number of tries it took before we found a number greater than .75.*/
 
-function randomGame(){
-    let counter = 0;
+/* ## **mergeObjects**
 
-  let randomTimer = setInterval(function() {
-    number = Math.random();
-    counter = counter + 1;
+Write a function called ***mergeObjects*** that accepts two objects and returns a new object 
+which contains all the keys and values of the first object and second object.*/
 
-  if (number > 0.75) {
-    console.log(number);
-    clearInterval(randomTimer);
-    console.log("It took " + counter + " times to find a number greater than 0.75.");
-} else {
-    console.log(number);
-    }
-},1000)
+function mergeObjects(obj1, obj2){
+    let newObj = {...obj1, ...obj2};
+    return newObj;
+}
+// console.log(mergeObjects({a:2,e:3},{i:1,o:4,u:3}))
+
+
+/* ## **doubleAndReturnArgs**
+
+Write a function called ***doubleAndReturnArgs*** which accepts an array and a variable number 
+of arguments. The function should return a new array with the original array values and all of 
+additional arguments doubled.*/
+
+function doubleAndReturnArgs(arr, ...args) {
+    const argsArr = [...args];
+    const doubleArgs = argsArr.map(function(val) {
+        return val * 2;
+    });
+    const combined = [...arr, doubleArgs];
+    return combined;
+}
+// console.log(doubleAndReturnArgs([1,2,3,4],5,6))
+
+/* ## **Slice and Dice!**
+
+For this section, write the following functions using rest, spread and refactor these functions 
+to be arrow functions!
+
+Make sure that you are always returning a new array or object and not modifying the existing 
+inputs.*/
+
+/** remove a random element in the items array
+and return a new array without that item.
+
+function removeRandom(items) {
+
+}*/
+
+const removeRandom = (items) => {
+    let indexForRemoval = Math.floor(Math.random() * items.length);
+    return [...items.slice(0,indexForRemoval), ...items.slice(indexForRemoval + 1)];
+}
+// console.log(removeRandom(["a","b","c","d","e","f","g"]));
+
+
+/** Return a new array with every item in array1 and array2. 
+
+function extend(array1, array2) {
+
+}*/
+
+const extend = (arr1, arr2) => {
+    return [...arr1, ...arr2];
+}
+// console.log(extend(["a","b","c"],["d","e","f","g"]))
+
+/** Return a new object with all the keys and values
+from obj and a new key/value pair 
+
+function addKeyVal(obj, key, val) {
+
+}*/
+
+const addKeyVal = (obj, key, val) => {
+    const newObj = {...obj};
+    newObj[key] = val;
+    return newObj;
+}
+// console.log(addKeyVal({a:1,b:2,c:3},{d:4}))
+
+
+/** Return a new object with a key removed.
+
+function removeKey(obj, key) {
+
+}*/
+
+const removeKey = (obj, key) => {
+    const newObj = {...obj};
+    delete newObj[key];
+    return newObj;
+}
+// console.log(addKeyVal({a:1,b:2,c:3},{c:3}))
+
+/** Combine two objects and return a new object. 
+
+function combine(obj1, obj2) {
+
+}*/
+
+const combine = (obj1, obj2) => {
+    return {...obj1, ...obj2};
+}
+// console.log(combine({a:1,b:2,c:3},{d:1,e:2,f:3}))
+
+/** Return a new object with a modified key and value.
+
+function update(obj, key, val) {
+
+}*/
+
+const update = (obj, key, val) => {
+    let newObj = {...obj};
+    newObj[key] = val;
+    return newObj;
 }
