e97ac11605f3d4a089e364a9d8c5b96349b3545f
aidataml <52681210+aidataml@users.noreply.github.com>
2023-09-19 16:57:05 -0400

    Initial upload of exercise for maps and sets

--- script.js
+++ script.js
@@ -1,36 +1,62 @@
-/* Write a function called countdown that accepts a number as a parameter and every 1000 milliseconds 
-decrements the value and console.logs it. Once the value is 0 it should log DONE! and stop.*/
+/* ****Maps and Sets Exercise****
 
-function countdown(number){
-  let countdownTimer = setInterval(function() {
-    number = number - 1;
+**Quick Question #1**
 
-  if (number <= 0) {
-    clearInterval(countdownTimer);
-    console.log("DONE!");
-} else {
-    console.log(number);
+What does the following code return? */
+
+new Set([1,1,2,2,3,4]) // returns Set(4) {1, 2, 3, 4}
+
+
+/* **Quick Question #2**
+
+What does the following code return? */
+
+//[...new Set("referee")].join("") // returns "ref"
+
+
+/* **Quick Questions #3**
+
+What does the Map ***m*** look like after running the following code?*/
+
+let m = new Map();
+m.set([1,2,3], true);
+m.set([1,2,3], false); /* returns Map(2) {Array(3) => true, Array(3) => false}
+[[Entries]]
+0 : {Array(3) => true}
+1 : {Array(3) => false}
+size : 2
+[[Prototype]] : Map*/
+
+/* **hasDuplicate**
+
+Write a function called hasDuplicate which accepts an array and returns true or false if that array contains a duplicate */
+
+const hasDuplicate = arr => new Set(arr).size !== arr.length;
+
+hasDuplicate([1,3,2,1]) // true
+hasDuplicate([1,5,-1,4]) // false
+
+
+/* **vowelCount**
+
+Write a function called vowelCount which accepts a string and returns a map where the keys are numbers and the values
+are the count of the vowels in the string.*/
+
+function vowelCount(str){
+    const vowels = 'aeiouAEIOU';
+    const m = new Map();
+
+    for (let i = 0; i < str.length; i++) {
+        let char = str[i]
+        if (vowels.includes(char)) {
+            !m.has(char) ? m.set(char, 1) : m.set(char, m.get(char) + 1);
+        }
     }
-},1000)
+
+    for (const [key, value] of m.entries()) {
+        console.log(`${key} = ${value}`);
+    }
 }
 
-/* Write a function called randomGame that selects a random number between 0 and 1 every 1000 milliseconds 
-and each time that a random number is picked, add 1 to a counter. If the number is greater than .75, stop 
-the timer and console.log the number of tries it took before we found a number greater than .75.*/
-
-function randomGame(){
-    let counter = 0;
-
-  let randomTimer = setInterval(function() {
-    number = Math.random();
-    counter = counter + 1;
-
-  if (number > 0.75) {
-    console.log(number);
-    clearInterval(randomTimer);
-    console.log("It took " + counter + " times to find a number greater than 0.75.");
-} else {
-    console.log(number);
-    }
-},1000)
-}
+vowelCount('awesome') // Map { 'a' => 1, 'e' => 2, 'o' => 1 }
+vowelCount('Colt') // Map { 'o' => 1 }
