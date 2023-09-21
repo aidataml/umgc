7affc17e220b8288f43c25bd227471619f8a4500
aidataml <52681210+aidataml@users.noreply.github.com>
2023-09-21 12:09:14 -0400

    Initial upload of ES2015 OOP Classes

--- script.js
+++ script.js
@@ -1,36 +1,153 @@
-/* Write a function called countdown that accepts a number as a parameter and every 1000 milliseconds 
-decrements the value and console.logs it. Once the value is 0 it should log DONE! and stop.*/
+/* Car ****OO Challenge****
 
-function countdown(number){
-  let countdownTimer = setInterval(function() {
-    number = number - 1;
+## **Part One**
 
-  if (number <= 0) {
-    clearInterval(countdownTimer);
-    console.log("DONE!");
-} else {
-    console.log(number);
+Create a class for vehicle. Each vehicle instance should have the following properties:
+
+- make
+- model
+- year
+
+Each vehicle instance should have access to a method called ***honk***, which returns the 
+string “Beep.”
+
+let myFirstVehicle = new Vehicle("Honda", "Monster Truck", 1999);
+myFirstVehicle.honk(); // "Beep."
+
+Each vehicle instance should have a method called toString, which returns the string containing 
+the make, model and year.
+
+let myFirstVehicle = new Vehicle("Honda", "Monster Truck", 1999);
+myFirstVehicle.toString(); // "The vehicle is a Honda Monster Truck from 1999."
+*/
+
+class Vehicle {
+    constructor (make, model, year) {
+    this.make = make;
+    this.model = model;
+    this.year = year;
     }
-},1000)
+
+    honk() {
+        return "Beep.";
+    }
+
+    toString() {
+        return `The vehicle is a ${this.make} ${this.model} from ${this.year}.`;
+    }
 }
 
-/* Write a function called randomGame that selects a random number between 0 and 1 every 1000 milliseconds 
-and each time that a random number is picked, add 1 to a counter. If the number is greater than .75, stop 
-the timer and console.log the number of tries it took before we found a number greater than .75.*/
+let myFirstVehicle = new Vehicle("Honda", "Monster Truck", 1999);
+console.log(myFirstVehicle.honk()); // "Beep."
+console.log(myFirstVehicle.toString()); // "The vehicle is a Honda Monster Truck from 1999."
 
-function randomGame(){
-    let counter = 0;
+/* **Part Two**
 
-  let randomTimer = setInterval(function() {
-    number = Math.random();
-    counter = counter + 1;
+Create a class for a car. The ***Car*** class should inherit from ***Vehicle*** and each car 
+instance should have a property called ***numWheels*** which has a value of 4.
 
-  if (number > 0.75) {
-    console.log(number);
-    clearInterval(randomTimer);
-    console.log("It took " + counter + " times to find a number greater than 0.75.");
-} else {
-    console.log(number);
+let myFirstCar = new Car("Toyota", "Corolla", 2005);
+myFirstCar.toString(); // "The vehicle is a Toyota Corolla from 2005."
+myFirstCar.honk();     // "Beep."
+myFirstCar.numWheels;  // 4*/
+
+class Car extends Vehicle {
+    constructor(make, model, year) {
+        super(make, model, year);
+        this.numWheels = 4;
     }
-},1000)
 }
+
+let myFirstCar = new Car("Toyota", "Corolla", 2005);
+console.log(myFirstCar.toString()); // "The vehicle is a Toyota Corolla from 2005."
+console.log(myFirstCar.honk());     // "Beep."
+console.log(myFirstCar.numWheels);  // 4
+
+/* **Part Three**
+
+Create a class for a Motorcycle. This class should inherit from ***Vehicle*** and each 
+motorcycle instance should have a property called ***numWheels*** which has a value of 2. 
+It should also have a ***revEngine*** method which returns “VROOM!!!”
+
+let myFirstMotorcycle = new Motorcycle("Honda", "Nighthawk", 2000);
+
+myFirstMotorcycle.toString();
+// "The vehicle is a Honda Nighthawk from 2000."
+
+myFirstMotorcycle.honk();     // "Beep."
+myFirstMotorcycle.revEngine(); // "VROOM!!!"
+myFirstMotorcycle.numWheels;  // 2
+*/
+
+class Motorcycle extends Vehicle {
+    constructor(make, model, year){
+        super(make, model, year);
+        this.numWheels = 2;
+    }
+
+    revEngine () {
+        return "VROOM!!! :)";
+    }
+}
+
+let myFirstMotorcycle = new Motorcycle("Honda", "Nighthawk", 2000);
+
+console.log(myFirstMotorcycle.toString()); // "The vehicle is a Honda Nighthawk from 2000."
+console.log(myFirstMotorcycle.honk());     // "Beep."
+console.log(myFirstMotorcycle.revEngine()); // "VROOM!!!"
+console.log(myFirstMotorcycle.numWheels);  // 2
+
+/* **Part Four**
+
+Create a class for a Garage. It should have a property called ***vehicles*** which will store 
+an array of vehicles, and a property called ***capacity*** which is a number indicating how 
+many vehicles will fit in the garage. When you create a garage, ***vehicles*** will always be 
+empty; you only need to provide the ***capacity***.
+
+A garage should also have an ***add*** method, which attempts to add a vehicle to the array 
+of vehicles. However, if you try to add something which is *not* a vehicle, the garage should 
+return the message “Only vehicles are allowed in here!”. Also, if the garage is at capacity, 
+it should say “Sorry, we’re full.”
+
+let garage = new Garage(2);
+garage.vehicles; // []
+garage.add(new Car("Hyundai", "Elantra", 2015)); // "Vehicle added!"
+garage.vehicles; // [Car]
+garage.add("Taco"); // "Only vehicles are allowed in here!"
+
+garage.add(new Motorcycle("Honda", "Nighthawk", 2000));
+// "Vehicle added!"
+garage.vehicles; // [Car, Motorcycle]
+
+garage.add(new Motorcycle("Honda", "Nighthawk", 2001));
+// "Sorry, we're full."
+*/
+
+class Garage {
+    constructor (capacity) {
+        this.vehicles = [];
+        this.capacity = capacity;
+    }
+
+    add (vehicleAddition) {
+        if (!(vehicleAddition instanceof Vehicle)) {
+            return "Only vehicles are allowed in here!";
+        } 
+
+        if (this.vehicles.length >= this.capacity) {
+            return "Sorry, we're full."
+        }
+
+        this.vehicles.push(vehicleAddition);
+        return "Vehicle added!"
+    }
+}
+
+let garage = new Garage(2);
+console.log(garage.vehicles); // []
+console.log(garage.add(new Car("Hyundai", "Elantra", 2015))); // "Vehicle added!"
+console.log(garage.vehicles); // [Car]
+console.log(garage.add("Taco")); // "Only vehicles are allowed in here!"
+console.log(garage.add(new Motorcycle("Honda", "Nighthawk", 2000))); // "Vehicle added!"
+console.log(garage.vehicles); // [Car, Motorcycle]
+console.log(garage.add(new Motorcycle("Honda", "Nighthawk", 2001))); // "Sorry, we're full."
